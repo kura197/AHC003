de6ba5db6cdcb8be5d5424edb98c414b655c7a8e
kuramochi <kura19971106@gmail.com>
2021-05-24 15:54:07 +0900

    support concurrent test execution

--- test.py
+++ test.py
@@ -1,32 +1,42 @@
 import os
 import subprocess, sys
 import re
+import pdb
 
 TESTER = '/home/kura/Documents/atcoder/AHC003/tools/target/release/tester'
 TEST_DIR = '/home/kura/Documents/atcoder/AHC003/tools/in'
 ANSWER = '/home/kura/Documents/atcoder/AHC003/answer'
 
-TEST = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90]
+#TEST = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90]
+TEST = [0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95]
 
-def test(test_num):
+def run_process(test_num):
     cmd = TESTER 
     arg1 = os.path.join(TEST_DIR, '{:04}.txt'.format(test_num))
     arg2 = ANSWER
     #print(cmd)
-    cp = subprocess.run([cmd, arg1, arg2], encoding='utf-8', stderr=subprocess.PIPE, stdout=subprocess.DEVNULL)
-    if cp.returncode != 0:
-        print('cmd failed.', file=sys.stderr)
-        sys.exit(1)
-    return cp.stderr
+    #proc = subprocess.run([cmd, arg1, arg2], encoding='utf-8', stderr=subprocess.PIPE, stdout=subprocess.DEVNULL)
+    proc = subprocess.Popen([cmd, arg1, arg2], encoding='utf-8', stderr=subprocess.PIPE, stdout=subprocess.DEVNULL)
+    return proc
 
 def main():
     total = 0
+    proc_list = []
+
     for t in TEST:
-        output = test(t)
-        #print(output, end='')
-        score = int(re.sub(r"\D", "", output))
+        proc = run_process(t)
+        proc_list.append((t, proc))
+
+    for t, proc in proc_list:
+        proc.wait()
+        if proc.returncode != 0:
+            print('cmd failed.', file=sys.stderr)
+            sys.exit(1)
+        #print(proc.communicate()[1])
+        score = int(re.sub(r"\D", "", proc.communicate()[-1]))
         print(f'test {t} : {score}')
         total += score
+
     print()
     print(f'total score : {total}.')
     print(f'average score : {total // len(TEST)}.')
